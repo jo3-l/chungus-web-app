9f75eebd8d9d5d2733a995175184fd471bc30827
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-03-25 12:10:56 -0700

    styling and nicknames

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
 	<div id="app">
-		<h1>make a chungus</h1>
-		Chungus name: <input v-model="name" />
-		<p>Your chungus name is: {{ name }}</p>
+		<h1>Make a Chungus</h1>
+		<strong>Chungus name: </strong> <input v-model="name" placeholder="enter le name" />
+		<p>
+			Your chungus name is: <strong>{{ name }}</strong>
+		</p>
 
+		<strong>Chungus size: </strong>
 		<select v-model="selected">
 			<option v-for="size in sizes" :key="size">
 				{{ size }}
@@ -14,10 +17,27 @@
 			Your preferred chungus size is: <strong>{{ selected }}</strong>
 		</p>
 		<img :src="chungusImageURL" height="300" />
+
+		<p>
+			Your chungus needs a nickname! Here is our suggestion: <strong>{{ chungusNickname }}</strong>
+		</p>
 	</div>
 </template>
 
 <script>
+const descriptors = [
+	'smelly',
+	'strong',
+	'buff',
+	'chunky',
+	'not real',
+	'real',
+	'substantial',
+	'colossal',
+	'inflated',
+	'big',
+];
+
 export default {
 	data() {
 		return {
@@ -42,17 +62,24 @@ export default {
 			const images = require.context('./assets', false, /\.png$/);
 			return images(`./${this.selected}chungus.png`);
 		},
+
+		chungusNickname() {
+			const descriptor = descriptors[Math.floor(Math.random() * descriptors.length)];
+			return `${this.selected}-sized ${descriptor} chungus`;
+		},
 	},
 };
 </script>
 
 <style>
-#app {
+body {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+	text-align: left;
+	color: #dddddd;
 	margin-top: 60px;
+	margin-left: 60px;
+	background-color: #141414;
 }
 </style>
